1067db80de713b807cdecaa0a4be171004d331b7
MinhQuan Le <mqle9137@gmail.com>
2025-10-02 18:05:08 -0400

    Revert clean up for table_operations.py

--- backend/python-ag-grid-backend/database.py
+++ backend/python-ag-grid-backend/database.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # database.py
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -15,4 +16,23 @@ def get_connection():
         password=db_password,
         port=db_port,
         cursor_factory=RealDictCursor
+=======
+# database.py
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+db_host = 'sportsdatabase.c9wy48s2kma9.us-east-2.rds.amazonaws.com'
+db_name = 'sportsdatabase'
+db_user = 'postgres'
+db_password = 'sportsanalyticspssword'
+db_port = 5432
+
+def get_connection():
+    return psycopg2.connect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        port=db_port,
+        cursor_factory=RealDictCursor
+>>>>>>> 305e8bb (Barebone gradio chatbot page - Embedded as Iframe)
     )

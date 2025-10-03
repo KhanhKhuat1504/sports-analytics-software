54a2450327790115e372d456c72e2f6b4b72be2e
William Schneider <was103@case.edu>
2025-10-03 17:59:12 -0400

    StrictMode, BrowserRouter (optional), & AuthProvider added to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"
 import MainContent from "./components/MainContent/MainContent";
 // import AnalyticsDashboard from "./components/AnalyticsDashboard/AnalyticsDashboard";
 // import AllTeamsView from "./components/AllTeamsView/AllTeamsView";
@@ -6,23 +7,28 @@ import NavBar from "./components/NavBar/NavBar";
 import AppLayout from './components/AppLayout/AppLayout';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import { AuthProvider } from './contexts/AuthContext';
+import { StrictMode } from 'react';
 
 function App() {
   return (
-    <Router>
-      {/* <NavBar /> */}
-      <Routes>
-       <Route path="/" element={<Login />} />
-       <Route path="/register" element={<Register />} />
-//         All Routes within the AppLayout have the sidebar
-        <Route element={<AppLayout />}>
-            <Route path="/tables" element={<MainContent />} />
-            <Route path="/tables/:selectedTable" element={<MainContent />} />
-            <Route path="/analytics" element={<div>Test Analytics Route</div>} />
-            <Route path="/ai-assistant" element={<div>Test AI Assistant Route</div>} />
-        </Route>
-      </Routes>
-    </Router>
+      <StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                {/* <NavBar /> */}
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route element={<AppLayout />}>
+                        <Route path="/tables" element={<MainContent />} />
+                        <Route path="/tables/:selectedTable" element={<MainContent />} />
+                        <Route path="/analytics" element={<div>Test Analytics Route</div>} />
+                        <Route path="/ai-assistant" element={<div>Test AI Assistant Route</div>} />
+                    </Route>
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
+    </StrictMode>
   );
 }
 
